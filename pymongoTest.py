25fa16f087009523fa9b863a72080c682b0722cf
newyoung124 <newyoung124@naver.com>
2024-04-17 00:41:58 +0900

    read

--- pymongoTest.py
+++ pymongoTest.py
@@ -4,7 +4,28 @@ client = pymongo.MongoClient('mongodb://localhost:27017/')
 db = client["disciplineTracer"]
 collection = db["discipline"]
 
-data = {"name": "John", "age": 30, "city": "New York"}
+
+discipline_booleans = []
+
+for i in range(365) :
+    discipline_booleans.append(0)
+
+
+
+
+discipline_comments = []
+
+for i in range(365) :
+    discipline_comments.append("")
+
+
+
+
+data = {"discipline":"read", "yes_or_no": discipline_booleans, "comment": discipline_comments}
+
+print(data)
+
+
 
 # Insert a single document
 result = collection.insert_one(data)
---
+++ pymongoTest_read.py
@@ -0,0 +1,12 @@
+import pymongo
+
+client = pymongo.MongoClient('mongodb://localhost:27017/')
+db = client["disciplineTracer"]
+collection = db["discipline"]
+
+data = {"discipline":"read"}
+
+
+# Insert a single document
+result = collection.find_one(data)
+print("Inserted document ID:", result)
