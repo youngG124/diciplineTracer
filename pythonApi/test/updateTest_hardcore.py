db7492d1f74f96dcce9289077c3ea5024c85b3b9
newyoung124 <newyoung124@naver.com>
2024-04-29 22:39:49 +0900

    add_a_day

--- pythonApi/test/updateTest_hardcore.py
+++ pythonApi/test/updateTest_hardcore.py
@@ -4,12 +4,16 @@ client = pymongo.MongoClient('mongodb://localhost:27017/')
 db = client["disciplineTracer"]
 collection = db["discipline"]
 
+def add_day(discipline_name) :
+    discipline_search = {"name":discipline_name}
+    find_one_result = collection.find_one(discipline_search)
 
-data = {"name":"read"}
+    a = find_one_result.get("yes_or_no")
+    a.insert(0, {"yes_or_no":0, "comment":""})
 
-find_one_result = collection.find_one(data)
+    result = collection.update_one({"name":discipline_name}, {"$set": {"yes_or_no": a}})
 
-a = find_one_result.get("yes_or_no")
-a.insert(0, {"yes_or_no":1, "comment":"second time"})
+disciplines = ["read", "run", "clean", "push-ups", "cold shower"]
 
-result = collection.update_one({"name":"read"}, {"$set": {"yes_or_no": a}})
+for i in disciplines :
+    add_day(i)
